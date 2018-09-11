36871e976b09f7b3937bfc7ebd27c25d05c1abb2
Weston Bassler <wbassler@mesosphere.com>
2018-09-11 16:23:03 -0400

    Updated the variables with descriptions so they could be imported to TF Reg

--- variables.tf
+++ variables.tf
@@ -1,11 +1,34 @@
-variable "remote_vpc_id" {}
-variable "remote_region" {}
-variable "remote_route_table_id" {}
-variable "main_region" {}
-variable "main_vpc_id" {}
-variable "main_subnet" {}
-variable "main_route_table_id" {}
-variable "cluster_name" {}
+variable "remote_vpc_id" {
+  description = "The VPC ID of the Remote VPC"
+}
+
+variable "remote_region" {
+  description = "The AWS Remote Region (ex: us-west-2, us-east-1)"
+}
+
+variable "remote_route_table_id" {
+  description = "ID of the Route Table for the Remote Region"
+}
+
+variable "main_vpc_id" {
+  description = "The VPC ID of the Main Region"
+}
+
+variable "main_region" {
+  description = "The Main Region where the Masters run (ex: us-west-2, us-east-1)"
+}
+
+variable "main_route_table_id" {
+  description = "ID of the Route Table for the Main Region"
+}
+
+variable "main_subnet" {
+  description = "The subnet range of the Main Region"
+}
+
+variable "cluster_name" {
+  description = "Name of the DC/OS Cluster"
+}
 
 variable "tags" {
   description = "Custom tags added to the resources created by this module"
